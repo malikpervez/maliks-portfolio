183718db758dc18fe85996a0b957f85c0700c569
Malik <mip12793@gmail.com>
2017-10-08 16:44:15 -0400

    added react nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar'
+import NavbarInstance from './components/Navbar'
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="container portfoliobackground">
-        <Navbar />
+        <NavbarInstance />
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,34 @@
 import React,{ Component } from 'react';
 import '../css/Navbar.css'
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem
+} from 'react-bootstrap';
 
-export default class Navbar extends Component {
-  render(){
+export default class NavbarInstance extends Component {
+  render() {
     return (
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>About Me</a></li>
-        <li><a>My Work</a></li>
-        <li><a>Resume</a></li>
-        <li><a>Contact</a></li>
-      </ul>
-    )
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">Home</NavItem>
+          <NavItem eventKey={2} href="#">About Me</NavItem>
+          <NavDropdown eventKey={3} title="My Work" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    );
   }
 }
