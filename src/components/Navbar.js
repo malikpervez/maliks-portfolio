56928721334b9c36a8ecb10e7982715fe61450ae
Malik <mip12793@gmail.com>
2017-10-08 17:18:47 -0400

    customizing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,6 @@ export default class NavbarInstance extends Component {
   render() {
     return (
       <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
-          </Navbar.Brand>
-        </Navbar.Header>
         <Nav>
           <NavItem eventKey={1} href="#">Home</NavItem>
           <NavItem eventKey={2} href="#">About Me</NavItem>
@@ -24,8 +19,6 @@ export default class NavbarInstance extends Component {
             <MenuItem eventKey={3.1}>Action</MenuItem>
             <MenuItem eventKey={3.2}>Another action</MenuItem>
             <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
           </NavDropdown>
         </Nav>
       </Navbar>
