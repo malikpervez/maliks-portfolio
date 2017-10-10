86961e631eae6a278b09e3e25ed4df15c5d6ed42
Malik <mip12793@gmail.com>
2017-10-10 13:29:12 -0400

    added text animation

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="portfoliobackground">
-        <NavbarInstance />
-            <div className='headline'>Malik Pervez</div>
+          <h1 data-heading="i">Malik</h1>
             <div className='profile-box container'>
             </div>
       </div>
