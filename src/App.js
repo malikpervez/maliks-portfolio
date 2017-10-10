f1fbfbca7fa98f9c6bc390291aec8fbf81ccddb7
Malik <mip12793@gmail.com>
2017-10-10 14:05:45 -0400

    added github and linked

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="portfoliobackground">
+        <header>
+          <a href='http://www.linkedin.com'>
+            <img src ={require('./images/linked.png')}></img>
+          </a>
+          <a href= 'http://www.github.com'>
+          <img src ={require('./images/github.png')}></img>
+          </a>
+        </header>
           <h1 data-heading="i">Malik</h1>
             <div className='profile-box container'>
             </div>
+
       </div>
     );
   }
