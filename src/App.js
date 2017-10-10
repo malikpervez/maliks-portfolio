4389ff5e8a48b164a3d1abe5ed202e961894993c
Malik <mip12793@gmail.com>
2017-10-10 17:42:31 -0400

    landing page finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import NavbarInstance from './components/Navbar'
+import { Fade } from 'react-reveal';
+
 import './css/App.css';
 
 class App extends Component {
@@ -7,17 +9,27 @@ class App extends Component {
     return (
       <div className="portfoliobackground">
         <header>
-          <a className='links' href='http://www.linkedin.com'>
-            <img src ={require('./images/linked.png')}></img>
-          </a>
-          <a className ='links' href= 'http://www.github.com'>
-          <img src ={require('./images/github.png')}></img>
-          </a>
+          <Fade left delay={1000}>
+            <a className='links' href='http://www.linkedin.com'>
+              <img src ={require('./images/linked.png')}></img>
+            </a>
+          </Fade>
+          <Fade left delay={1500}>
+            <a className ='links' href= 'http://www.github.com'>
+              <img src ={require('./images/github.png')}></img>
+            </a>
+          </Fade>
+          <Fade left delay={2000}>
+            <a className ='links' href= 'http://www.github.com'>
+              <img src ={require('./images/contactme.png')}></img>
+            </a>
+          </Fade>
         </header>
-          <h1 data-heading="i">Malik</h1>
-            <div className='profile-box container'>
-              <div className="button">Full Stack Developer</div>
-            </div>
+        <h1 data-heading="i">Malik</h1>
+        <div className='profile-box container'>
+          <div className="button">Full Stack Developer</div>
+        </div>
+
 
       </div>
     );
