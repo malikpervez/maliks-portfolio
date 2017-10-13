95649c082616420a43cae771e1711f748a995ba3
Malik <mip12793@gmail.com>
2017-10-13 19:27:49 -0400

    mywork media q

--- src/components/Mywork.js
+++ src/components/Mywork.js
@@ -15,7 +15,7 @@ class Mywork extends Component {
             <div className='ontheflybody'>On the fly is a application that takes the tedious tasks of owning a restuarant and simplifies them.
               Taking reservations and managing bar as well as kitchen inventory is crticial when running a business in hospitality. Why not make it easy?
             </div>
-            <ul className='ontheflybody2'>Features
+            <ul className='ontheflybody2'>Features:
               <li>realtime database</li>
               <li>inventory tracking</li>
               <li>booking reservations</li>
@@ -28,7 +28,7 @@ class Mywork extends Component {
               Have you gotten tired of creating a new email for weekly emails with parents, struggling to find the right words to use?
               With Teacher's Assistant you no longer have to. Mark individual students with different reports and send a template generated email at the click of a button. What will you do with those extra hours each week? You decide.
             </div>
-            <ul className='teacherspetbody2'>Features
+            <ul className='teacherspetbody2'>Features:
               <li>Charts and graphs for data tracking</li>
               <li>Generated Reports</li>
               <li>Templated Emails</li>
@@ -39,7 +39,8 @@ class Mywork extends Component {
           <img className='onthefly' src={require('../images/onthefly.jpg')}></img>
           <a href='https://github.com/malikpervez/On_the_fly'>github</a>
           <img className='teacherspet' src={require('../images/teacherpet.jpg')}></img>
-          <a href= 'https://github.com/Dramalho7/teachers-pet'>github</a>        </div>
+          <a href= 'https://github.com/Dramalho7/teachers-pet'>github</a>
+        </div>
       </div>
     )
   }
